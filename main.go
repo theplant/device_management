7d1f2ce8154c2a41255cbf9681b7d1b63cb2af64
Felix Sun <sunfmin@gmail.com>
2015-08-14 17:22:38 +0800

    Add translation

--- main.go
+++ main.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-
 	"github.com/qor/qor"
 	"github.com/qor/qor/admin"
+	"github.com/qor/qor/i18n"
+	"github.com/qor/qor/i18n/backends/database"
 	"github.com/theplant/device_management/db"
+	"log"
+	"net/http"
 )
 
 func main() {
 	adm := admin.New(&qor.Config{DB: &db.DB})
-
+	adm.SetAuth(&Auth{})
 	device := adm.AddResource(&db.Device{}, &admin.Config{Menu: []string{"设备管理"}})
 	device.Meta(&admin.Meta{Name: "Category", Type: "select_one", Collection: []string{"自有设备", "消耗品", "客户设备"}})
 
@@ -38,6 +39,11 @@ func main() {
 	adm.AddResource(&db.WareHouse{}, &admin.Config{Menu: []string{"设备管理"}})
 
 	adm.AddResource(&db.Consumable{}, &admin.Config{Menu: []string{"设备管理"}})
+
+	I18nBackend := database.New(&db.DB)
+	// config.I18n = i18n.New(I18nBackend)
+	adm.AddResource(i18n.New(I18nBackend), &admin.Config{Menu: []string{"系统设置"}})
+
 	adm.MountTo("/admin", http.DefaultServeMux)
 
 	log.Println("Starting Server at 9000.")
@@ -66,4 +72,30 @@ func formatedDeviceName(resource interface{}, ctx *qor.Context) interface{} {
 	}
 
 	return text
+}
+
+type Auth struct{}
+
+func (Auth) LoginURL(c *admin.Context) string {
+	return "/"
+}
+
+func (Auth) LogoutURL(c *admin.Context) string {
+	return "/"
+}
+
+func (Auth) GetCurrentUser(c *admin.Context) qor.CurrentUser {
+
+	return &User{}
+}
+
+type User struct {
+}
+
+func (u User) AvailableLocales() []string {
+	return []string{"zh_CN"}
+}
+
+func (u User) DisplayName() string {
+	return "管理员"
 }
